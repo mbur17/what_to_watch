96d4e83d0032c0b3694bad50fb697c571c956d38
Maxim Buryakovsky <mbur1717@gmail.com>
2025-04-14 22:55:56 +0300

    setup api

--- opinions_app/models.py
+++ opinions_app/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from . import db
 
@@ -8,5 +8,23 @@ class Opinion(db.Model):
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    timestamp = db.Column(db.DateTime,
+                          index=True,
+                          default=datetime.now(timezone.utc)
+                          )
     added_by = db.Column(db.String(64))
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title,
+            text=self.text,
+            source=self.source,
+            timestamp=self.timestamp,
+            added_by=self.added_by
+        )
+
+    def from_dict(self, data):
+        for field in ['title', 'text', 'source', 'added_by']:
+            if field in data:
+                setattr(self, field, data[field])
